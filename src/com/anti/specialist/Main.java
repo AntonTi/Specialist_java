8bc871506ca57fbbf453c1c661c7a44a6b10a031
Antony <antony@mail.com>
2021-08-29 17:23:55 +0300

    added Task 'MultiplicationTable' and 'Guess the number'

--- src/com/anti/specialist/Main.java
+++ src/com/anti/specialist/Main.java
@@ -5,10 +5,16 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Singleton st1 = Singleton.getInstance();
+        /*Singleton st1 = Singleton.getInstance();
         Singleton st2 = Singleton.getInstance();
         st1.data = "Hello";
-        System.out.println(st2.data);
+        System.out.println(st2.data);*/
+
+        /*MultiplicationTable.multiplTable_10x10();
+        MultiplicationTable.multiplTable(5, 4);*/
+
+        GuessTheNumber.guessNumber();
+
 
     }
 
